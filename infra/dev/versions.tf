9ab5d62bafea132acdfb180ad1066001a1372641
mmestres-cw <mmestres91@gmail.com>
2025-07-11 18:59:13 -0400

    style(terraform): auto-format versions.tf via pre-commit

--- infra/dev/versions.tf
+++ infra/dev/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.8.5"
   required_providers {
-    aws    = { source = "hashicorp/aws",    version = "~> 5.50" }
-    random = { source = "hashicorp/random", version = "~> 3.6"  }
+    aws    = { source = "hashicorp/aws", version = "~> 5.50" }
+    random = { source = "hashicorp/random", version = "~> 3.6" }
   }
 }
